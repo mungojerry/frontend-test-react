d1b7378e84dac6bc39f7f6c8664b1ad981d38059
mattfolkard <matt.folkard@matmi.com>
2018-08-21 16:54:04 +0100

    Wired up app to components + added props

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,65 @@
-import React, { Component } from 'react';
+import './app.css';
+import AddToDo from '../addtodo/addtodo';
 import Logo from '../../assets/logo.svg';
-import './App.css';
-import AddToDo from '../addToDo/';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import TodoList from '../todolist/todolist';
+import actions from '../../actions';
+import { connect } from 'react-redux';
+import { faTrashAlt, faTimes, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-const App = () => (
-  <div class="app">
-    <div class="app-header">
-      <Logo width={50} height={50} ></Logo>
-      <div className="app-title">To Do List</div>
-      <div className="app-intro">The worlds most exciting to-do app</div>
-    </div>
-    <div class="app-content">
-      <AddToDo submitToDo={() => { }} />
-    </div>
-  </div>
-);
+library.add(faTrashAlt, faTimes, faCheckSquare);
 
-export default App;
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <Logo width={50} height={50} ></Logo>
+          <div className="app-title">To Do List</div>
+          <div className="app-intro">The worlds most exciting to-do app</div>
+        </div>
+        <div className="app-content">
+          <AddToDo submitToDo={this.props.submitToDo} />
+          <TodoList todos={this.props.todos} deleteToDo={this.props.deleteToDo} />
+        </div>
+      </div>
+    );
+  }
+};
+
+App.propTypes = {
+  submitToDo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape(
+    {
+      id: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      createdAt: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      priority: PropTypes.number.isRequired,
+      tags: PropTypes.array.isRequired,
+      isDone: PropTypes.bool.isRequired,
+    },
+  )).isRequired,
+  deleteToDo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todos;
+
+const mapDispatchToProps = dispatch => ({
+  submitToDo: (text) => {
+    if (text) {
+      dispatch(actions.submitToDo(text));
+    }
+  },
+  deleteToDo: (id) => {
+    dispatch(actions.deleteToDo(id));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,12 +1,21 @@
 /* global it, expect */
 
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import App from './app';
 import React from 'react';
 import { configure, shallow } from 'enzyme';
-
+import store from '../../store';
+import { Provider } from 'react-redux';
+import { initialState } from '../../reducers';
 configure({ adapter: new Adapter() });
+
+
 it('renders app without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+  const component = shallow(<Provider store={store}>
+    <App state={initialState}
+      submitToDo={mockFunction}
+      todos={[]} />
+  </Provider>);
   expect(component.exists()).toEqual(true);
 });
